98109ed19d27bd780e618caf2165102c4107922d
jodiecouch <couchjodie@gmail.com>
2023-01-07 10:36:30 -0700

    prep assignments one and 2 completed.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        int grade;
+        string letter = "";
+
+        Console.WriteLine("Enter your grade as a percentage:");
+        grade = int.Parse(Console.ReadLine());
+
+        //Console.WriteLine("Hello Prep2 World!");
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations on passing.");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time, Loser.");
+        }
     }
 }
