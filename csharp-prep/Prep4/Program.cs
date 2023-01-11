8159c6102e48920f63fc7aae567aaffc40d3a12e
jodiecouch <couchjodie@gmail.com>
2023-01-10 18:31:00 -0700

    core requirements for prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        string input = "";
+        int userNumber;
+        List<int> numList = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        do
+        {
+            Console.WriteLine("Enter number:");
+            input = Console.ReadLine();
+            if (Int32.TryParse(input, out userNumber))
+            {
+                if (userNumber != 0)
+                {
+                    numList.Add(userNumber);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid entry. Try again.");
+            }
+
+        } while (userNumber != 0);
+
+        float sum = 0;
+        float counter = 0;
+        float avg = 0;
+        int max = 0;
+
+        foreach (int num in numList)
+        {
+            sum += num;
+            counter++;
+            if (num > max)
+            {
+                max = num;
+            }
+        };
+        avg = sum / counter;
+        Console.WriteLine($"Sum is {sum}. Average is {avg}. Maximum is {max}. Counter is {counter}.");
+
     }
 }
