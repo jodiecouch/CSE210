b35e089c83dfa55360330d1a8902b9d36f4b5400
jodiecouch <couchjodie@gmail.com>
2023-02-10 23:41:09 -0700

    cleaned up comments

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -7,24 +7,6 @@ public class Scripture
 
     private string _text;
 
-    /*
-    public Scripture()
-    {
-        _book = "John";
-        _chapter = "3";
-        _verses = "16";
-        _text = "text for John 3:16";
-    }
-
-    public Scripture(string book, string chapter, string verse)
-    {
-        _book = book;
-        _chapter = chapter;
-        _verses = verse;
-        _text = "text for this scripture";
-
-    }
-    */
     public Scripture(string book, string chapter, string verse, string text)
     {
         _book = book;
