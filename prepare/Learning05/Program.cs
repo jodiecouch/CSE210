5349a385b17b1c71257d9d0b19b37040128feb0e
jodiecouch <couchjodie@gmail.com>
2023-03-01 11:23:33 -0700

    week 9 learning

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square box = new Square("Yellow", 2);
+        shapes.Add(box);
+        Rectangle longBox = new Rectangle("Blue", 4, 6);
+        shapes.Add(longBox);
+        Circle roundShape = new Circle("Plum", 3);
+        shapes.Add(roundShape);
+
+        foreach (Shape s in shapes)
+        {
+            Console.WriteLine($"Shape color is {s.GetColor()} and area is {s.GetArea()}");
+        }
+
     }
 }
