8140330805c45b142718426251f893b580878f2a
jodiecouch <couchjodie@gmail.com>
2023-03-25 10:47:47 -0600

    foundation 1 in final

---
+++ final/Foundation1/Comment.cs
@@ -0,0 +1,7 @@
+using System;
+
+public class Comments
+{
+    private string _author;
+    private string _text;
+}
---
+++ final/Foundation1/Video.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Video
+{
+    private string _title;
+    private string _author;
+    private int _lengthInSeconds;
+    private List<Comment> _comments = new List<Comment>();
+
+}
