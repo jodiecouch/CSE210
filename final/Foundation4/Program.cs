6ad87027b899bdfa46bd85c77e2461d6dd9493b2
jodiecouch <couchjodie@gmail.com>
2023-03-29 13:31:01 -0600

    program 4 finished

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,14 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        //Console.WriteLine("Hello Foundation4 World!");
+        List<Activity> exercise = new List<Activity>();
+
         Run jog = new Run("3/29/2023", 2, .25);
-        Console.WriteLine(jog.GetSummary());
+        exercise.Add(jog);
 
         Cycle bike = new Cycle("3/20/2023", 25, 15);
-        Console.WriteLine(bike.GetSummary());
+        exercise.Add(bike);
 
         Swim swim = new Swim("3/25/2023", 30, 90);
-        Console.WriteLine(swim.GetSummary());
+        exercise.Add(swim);
+
+
+        exercise.ForEach(delegate (Activity act)
+        {
+            Console.WriteLine(act.GetSummary());
+        });
     }
 }
